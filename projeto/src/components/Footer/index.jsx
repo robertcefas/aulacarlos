637beb60d2b6505146ceda364e7aab620b9cfded
Your Name <you@example.com>
2026-04-17 20:41:33 -0300

    'oi'

---
+++ projeto/src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export default function Footer () {
+    return(
+        <footer>
+            <div>
+                <strong>Contabilidade Simples</strong> - CNPJ 00.000.000/0001
+                <p>Rua Exemplo, 123, - Centro - Salvador/BA</p>
+            </div>
+        </footer>
+    )
+}
---
+++ projeto/src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import { Link, NavLink } from 'react-router-dom'
+import './style.css'
+import logo from '../../assets./Logotipo.svg'
+
+
+export default function Footer () {
+    return(
+        <header>
+            <div>
+                <Link to="/">
+                <img src={logo} alt='Logo da empresa'/>
+                <span>Contabiliadade Simples</span>
+                </Link>
+
+                <nav>
+                    <NavLink to="/sobre-nos">Sobre nós</NavLink>
+                    <NavLink to="/fale-conosco">Fale conosco</NavLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
