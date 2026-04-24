bbfc8e77aa9667e9015d79285e84e33076f5e9fe
Your Name <you@example.com>
2026-04-23 21:31:18 -0300

    terminar

--- projeto/src/pages/Sobrenos/index.jsx
+++ projeto/src/pages/Sobrenos/index.jsx
@@ -1,17 +1,17 @@
-import './style.css'
+import './style.css';
 
 export default function SobreNos (){
-    <Selection>
+    <section>
         <h1>Sobre nós</h1>
         <p>
             Somos um escritório de contabilidade com foco em pequenas e mpedias empresas.
             Acrediramos que finanças claras e processos simples aceleram o crescimento.
         </p>
-        <url>
+        <ul>
             <li>+10 anos no mercado</li>
             <li>Equipe sênior e atendimento consultivo</li>
             <li>Parceria com ERPs e bancos digitais</li>
-        </url>
-    </Selection>
+        </ul>
+    </section>
     
 }
